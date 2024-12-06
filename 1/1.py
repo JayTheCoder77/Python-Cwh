cc2478ba6f41ba86718fa53cb1af29091c29a349
JAYANT <jayant@JAYANTs-Mac.local>
2024-12-06 04:23:33 -0800

    start from bro code chapter 6 arithmetic and math

---
+++ 1/1.py
@@ -0,0 +1,21 @@
+#strings
+first_name = "jay"
+print("first name")
+print(first_name)
+print(f"Helllo {first_name}")
+
+#int
+age = 18
+quantity = 3
+print(f"I am {age} years old")
+print(f"I have {quantity} mangoes")
+
+#float
+price = 10.7
+print(f"I have {price} $")
+
+#boolean
+
+is_student = True
+print(f"Are you a student? : {is_student}")
+
---
+++ 5/madlibs.py
@@ -0,0 +1,9 @@
+adj1 = input("enter adj1 descr.")
+noun1 = input("enter noun1 -> person place thing")
+verb1 = input("enter verb1 any action")
+adj2 = input("enter adj2 any descr.")
+
+print(f"Today I went to a {adj1} zoo")
+print(f"In an exhibit i saw a {noun1}")
+print(f"{noun1} was {adj2} and {verb1}")
+print(f"i was {adj2}")
