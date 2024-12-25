02e6265213dab1b9bed471a220ebe5a9efa5a90c
Jay <jay@Jays-Mac.local>
2024-12-25 22:14:39 +0530

    25 dec

---
+++ 39/a.py
@@ -0,0 +1,19 @@
+#module
+# import math as bro
+# from math import pi
+import example
+# print(help("math"))
+# print(math.pi)
+# print(bro.pi)
+# print(pi)
+
+
+result = example.pi
+print(result)
+
+result = example.square(3)
+print(result)
+result = example.cube(3)
+print(result)
+result = example.area(3)
+print(result)
